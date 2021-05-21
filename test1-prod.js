556f0742ca702c8d0aabba060d3334d42df8eb12
Joao Machado <machado@codacy.com>
2021-05-21 10:41:46 +0100

    Update test1-prod.js

--- test1-prod.js
+++ test1-prod.js
@@ -7,4 +7,6 @@ if(fooProd  === "bar") {
   console.log( 'sha from event' )
   
   console.log( 'sha checking event name for real with logs' )
+  
+  console.log( 'forcing correct commit uuid for the CLI' )
 }
